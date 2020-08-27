97482e7aa7a3686916f1477c2627dd644c220873
Gerry <gerry170205@163.com>
2020-08-27 14:54:11 +0800

    内容区域

---
+++ src/components/ContentMain/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import { Route, Switch } from 'react-router-dom'
+import { Router, Route, IndexRoute, hashHistory/* , Redirect */ } from 'react-router'
+
+export default class ContentMain extends Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={global.Order} />
+        <Route path="/hotel" component={global.Hotel} />
+        <Route path="/order" component={global.Order} />
+      </Router>
+    )
+  }
+}
