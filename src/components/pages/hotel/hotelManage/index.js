b91cc8dfa00e713cfeadeb113468883a45b092ba
Gerry <gerry170205@163.com>
2020-08-27 14:59:05 +0800

    酒店管理

---
+++ src/components/pages/hotel/hotelManage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class hotel extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+
+      <div>酒店</div>
+    )
+  }
+}
