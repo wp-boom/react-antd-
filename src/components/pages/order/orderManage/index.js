357ac7a81ee06c48aca0549ec8855c7a27ae3493
Gerry <gerry170205@163.com>
2020-08-27 14:58:42 +0800

    订单管理

---
+++ src/components/pages/order/orderManage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class order extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+
+      <div>订单</div>
+    )
+  }
+}
