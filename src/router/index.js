be3bbf16cf4c6451ec99bf5f319d8db81413ad7a
Gerry <gerry170205@163.com>
2020-08-28 14:33:10 +0800

    路径配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@ import Loadable from 'react-loadable' // Loadable 必须使用loading
 import Loading from '../components/Loading'
 
 global.Hotel = Loadable({
-  loader: () => import('../components/pages/hotelManage'),
+  loader: () => import('../components/pages/hotel/hotelManage'),
   loading: Loading,
 })
 
 global.Order = Loadable({
-  loader: () => import('../components/pages/orderManage'),
+  loader: () => import('../components/pages/order/orderManage'),
   loading: Loading,
 })
